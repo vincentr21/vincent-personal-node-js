736f57b5ce8a500af0b62f5f877ecd864cf35b5a
Vincent Ren <vincent@wyw.kr>
2016-09-02 12:53:59 -0400

    added basic static page layout, some imgs as well:

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
 
+//sets the port that the server will listen on
 app.set('port', (process.env.PORT || 5000));
 
+//tells nodejs where to find static files
+app.use(express.static(__dirname + '/public'));
+
+//views is directory for all template files
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+
 app.get('/', function (request, response) {
-  response.send('Hello Vincent! Now automatically deployed from Github!');
+//	response.render('about-me.html');
+	
+	//this is probably not the best way to do this, but i'll do for now
+	response.sendFile( __dirname + '/public/about-me.html');
 });
 
 app.listen(app.get('port'), function() {
