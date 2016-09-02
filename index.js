a0dc118fc3842e4df699e6a779e4c977788f3057
Vincent Ren <vincent@wyw.kr>
2016-09-02 19:19:24 -0400

    installed ejs, created basic template structure

--- index.js
+++ index.js
@@ -9,16 +9,38 @@ app.use(express.static(__dirname + '/public'));
 
 //views is directory for all template files
 app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 
 app.get('/', function (request, response) {
 //	response.render('about-me.html');
 	
 	//this is probably not the best way to do this, but i'll do for now
-	response.sendFile( app.get('views') + '/pages/about-me.html');
+//	response.sendFile( app.get('views') + '/pages/about-me.html');
+	
+	response.render('pages/about-me');
 });
+
+app.get('/blast-past', function (request, response) {
+	response.render('pages/blast-past');
+});
+
+app.get('/showcase', function (request, response) {
+	response.render('pages/showcase');
+});
+
+app.get('/about-website', function (request, response) {
+	response.render('pages/about-website');
+});
+
+app.get('/projects', function (request, response) {
+	response.render('pages/projects');
+});
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
